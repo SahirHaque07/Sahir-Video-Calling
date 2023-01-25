b4de61e25437b3a4ebc4c9efcd7dfab38d38698d
Sahir Haque <ayaanhaque07@gmail.com>
2023-01-25 13:07:14 +0530

    "Build Done"

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const io = require("socket.io")(server, {
 });
 
 app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname,"./client/build")));
 app.get('*',function(req,res){
 	res.sendFile(path.join(__dirname,"./client/build/index.html"));
